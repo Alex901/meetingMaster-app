fb7f55b36c7d1d15586601d960380e8b80f3c1a7
Alex901 <alexander.winberg.036@gmail.com>
2024-06-23 11:04:48 +0200

    fixad dumb error

--- contexts/EmployeeContext.jsx
+++ contexts/EmployeeContext.jsx
@@ -1,8 +1,8 @@
-import React, { createContext, useState, useEffect, useContext } from 'react';
+import React, { createContext, useContext, useState, useEffect } from 'react';
 
 const EmployeeContext = createContext();
 
-EmployeeProvider = ({ children }) => {
+const EmployeeProvider = ({ children }) => {
     const [employeeList, setEmployeeList] = useState([]);
 
     const addEmployee = (employee) => {
@@ -23,4 +23,4 @@ EmployeeProvider = ({ children }) => {
 
     const useEmployeeContext = () => useContext(EmployeeContext);
 
-    return { EmployeeProvider, useEmployeeContext };    
+    export { EmployeeProvider, useEmployeeContext };    
--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { MeetingProvider } from '../contexts/MeetingContext.jsx'
+import { EmployeeProvider } from '../contexts/EmployeeContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <MeetingProvider>
-    <App />
-    </MeetingProvider>
+    <EmployeeProvider>
+     <MeetingProvider>
+       <App />
+     </MeetingProvider>
+    </EmployeeProvider>
   </React.StrictMode>,
 )
