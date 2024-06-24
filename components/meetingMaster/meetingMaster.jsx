8880b0bedd56f49e61ce940686cf5333ae79b75a
Alexander Winberg <alexander.winberg.036@gmail.com>
2024-06-24 07:41:02 +0200

    Mede it clearer to see what tab is selected

--- components/meetingMaster/meetingMaster.jsx
+++ components/meetingMaster/meetingMaster.jsx
@@ -19,12 +19,40 @@ function MeetingMaster() {
             display="flex"
             justifyContent="center"
             alignItems="top"
+            position="absolute" // or use "fixed" if you want it to stay at the top when scrolling
+            top="50px" 
             height="auto"
             width="100vw"
         >
-            <Card sx={{ backgroundColor: '#ffffff', padding: '7px', height:'auto' }}>
+            <Card sx={{ backgroundColor: '#ffffff', padding: '7px', height: 'auto' }}>
                 <CardContent>
-                    <Tabs value={selectedTab} onChange={handleChange} sx={{ width: 'auto' }} >
+                    <Tabs
+                        value={selectedTab}
+                        onChange={handleChange}
+                        sx={{
+                            width: 'auto',
+                            '.MuiTab-root': { // Targeting the root of the Tab component
+                                '&.Mui-selected': { // Specifically targeting selected state of the tab
+                                    border: 'none',
+                                    color: 'purple',
+                                    backgroundColor: 'rgba(255, 165, 0, 0.5)',
+                                    borderRadius: '10px',
+                                    '&:active': { // Removing any styles for the active state
+                                        outline: 'none',
+                                        border: 'none',
+                                    },
+                                    '&:focus-visible': { // Specifically removing focus-visible outline or border
+                                        outline: 'none',
+                                        border: 'none',
+                                    },
+                                },
+                            },
+                            '.MuiTabs-indicator': { // Optionally, if you want to remove the indicator line as well
+
+                            },
+                        }}
+
+                    >
                         <Tab icon={<GroupsIcon />} label="Meetings" iconPosition='start' />
                         <Tab icon={<BadgeIcon />} label="Employees" iconPosition='start' />
                         <Tab icon={<BackupIcon />} label="Data" iconPosition='start' />
